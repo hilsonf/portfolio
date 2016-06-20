a056218420cc70451a3cce5467c256a9ee7cd5b6
HilsonF <hilson_francis@Hilsons-MacBook-Pro.local>
2016-06-20 17:13:56 -0400

    Added content on individual projects

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express 	= require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.use(express.static('public'));
 
@@ -7,17 +7,6 @@ app.get('*', function(req, res){
     res.sendFile('./public/index.html', { root: __dirname });
 }); 
 
-// Download Resume 
-app.get('/download',function(req, res){
-
-	console.log('RESULTS======  ', res.download('/public/downloads/resume.pdf'));
-
-// res.download('public/downloads/resume.pdf');
-	
-});
-
-var server = app.listen(process.env.PORT || 3000, function(){
+const server = app.listen(process.env.PORT || 3000, function(){
 	console.log('server listening on ', server.address().port)
 })
-
-//apples
